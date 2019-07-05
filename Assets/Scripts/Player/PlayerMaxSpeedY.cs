3ac40a6cc3904a00dd15f6acd23c77126e2d118a
IoT-Experts <info@iot-experts.ca>
2019-07-05 21:40:52 +0200

    Initial check-in of module SuperKong-V1.2

---
+++ Assets/Scripts/Player/PlayerMaxSpeedY.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMaxSpeedY : MonoBehaviour {
+	public float maxSpeed = 40;
+
+	Rigidbody2D rigid;
+	// Use this for initialization
+
+	void Awake () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigid.velocity.y <= -maxSpeed)
+			rigid.velocity = new Vector2 (rigid.velocity.x, Mathf.Lerp(rigid.velocity.y , -maxSpeed, 0.1f));
+	}
+}
